cdb1b236c4d90bd58d737e0c37804f83be3c3674
andy1445 <gaoandy1445@gmail.com>
2018-10-11 01:07:24 -0400

    made setup easier

---
+++ setup.sh
@@ -0,0 +1,14 @@
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt install python3-pip python3-tk
+pip3 install setuptools tensorflow-gpu keras==2.2.2 opencv-python
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+tar -xvf VOCtrainval_11-May-2012.tar
+tar -xvf VOCtest_06-Nov-2007.tar
+tar -xvf VOCtrainval_06-Nov-2007.tar
+
+# copy these lines for cudnn installation
+#sudo cp cuda/include/cudnn.h /usr/local/cuda/include
+#sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+#sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
