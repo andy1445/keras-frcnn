17e10d495b6f334e3a5614522a60177aee09f30f
Chameleon Cloud User <cc@nn.novalocal>
2018-10-25 01:46:12 +0000

    fixed_setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt install python3-pip python3-tk
-pip3 install setuptools tensorflow-gpu keras==2.2.2 opencv-python
+pip3 install setuptools 
+pip3 install tensorflow-gpu keras==2.2.2 opencv-python
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
@@ -12,3 +13,14 @@ tar -xvf VOCtrainval_06-Nov-2007.tar
 #sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 #sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 #sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+
+# setup vim.rc
+cat > ~/.vimrc << EOF
+set nocompatible
+filetype indent plugin on
+syntax on
+set autoindent
+set shiftwidth=4
+set expandtab
+color delek
+EOF
