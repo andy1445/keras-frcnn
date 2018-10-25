25769f874361044a2e33e0b5447d94ceb5a496e1
andy1445 <gaoandy1445@gmail.com>
2018-10-24 22:34:22 -0400

    reformat

--- train_frcnn.py
+++ train_frcnn.py
@@ -31,14 +31,17 @@ parser.add_option("--hf", dest="horizontal_flips", help="Augment with horizontal
                   action="store_true", default=False)
 parser.add_option("--vf", dest="vertical_flips", help="Augment with vertical flips in training. (Default=false).",
                   action="store_true", default=False)
-parser.add_option("--rot", "--rot_90", dest="rot_90", help="Augment with 90 degree rotations in training. (Default=false).",
+parser.add_option("--rot", "--rot_90", dest="rot_90",
+                  help="Augment with 90 degree rotations in training. (Default=false).",
                   action="store_true", default=False)
 parser.add_option("--num_epochs", type="int", dest="num_epochs", help="Number of epochs.", default=2000)
-parser.add_option("--config_filename", dest="config_filename", help="Location to store all the metadata related to the training (to be used when testing).",
+parser.add_option("--config_filename", dest="config_filename",
+                  help="Location to store all the metadata related to the training (to be used when testing).",
                   default="config.pickle")
 parser.add_option("--output_weight_path", dest="output_weight_path", help="Output path for weights.",
                   default='./model_frcnn.hdf5')
-parser.add_option("--input_weight_path", dest="input_weight_path", help="Input path for weights. If not specified, will try to load default weights provided by keras.")
+parser.add_option("--input_weight_path", dest="input_weight_path",
+                  help="Input path for weights. If not specified, will try to load default weights provided by keras.")
 
 (options, args) = parser.parse_args()
 
@@ -167,14 +170,16 @@ for epoch_num in range(num_epochs):
                 print('Average number of overlapping bounding boxes from RPN = {} for {} previous iterations'.format(
                     mean_overlapping_bboxes, epoch_length))
                 if mean_overlapping_bboxes == 0:
-                    print('RPN not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
+                    print(
+                        'RPN not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
 
             X, Y, img_data = next(data_gen_train)
             # train rpn on batch
             loss_rpn = model_rpn.train_on_batch(X, Y)
             # get region proposals
             P_rpn = model_rpn.predict_on_batch(X)
-            R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, K.image_dim_ordering(), use_regr=True, overlap_thresh=0.7, max_boxes=300)
+            R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, K.image_dim_ordering(), use_regr=True, overlap_thresh=0.7,
+                                       max_boxes=300)
             # note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
             X2, Y1, Y2, IouS = roi_helpers.calc_iou(R, img_data, C, class_mapping)
 
