8810cae0dbc0ff2b38db2832fbbcf2ca56a8f86f
andy1445 <gaoandy1445@gmail.com>
2018-10-24 23:02:14 -0400

    readded fixed batch norm

--- keras_frcnn/3dresnet.py
+++ keras_frcnn/3dresnet.py
@@ -47,16 +47,16 @@ def identity_block(input_tensor, kernel_size, filters, stage, block, trainable=T
     bn_name_base = 'bn' + str(stage) + block + '_branch'
 
     x = Convolution3D(nb_filter1, (1, 1, 1), name=conv_name_base + '2a', trainable=trainable)(input_tensor)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
 
     x = Convolution3D(nb_filter2, (kernel_size, kernel_size, kernel_size), padding='same', name=conv_name_base + '2b',
                       trainable=trainable)(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
 
     x = Convolution3D(nb_filter3, (1, 1, 1), name=conv_name_base + '2c', trainable=trainable)(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+    x = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
 
     x = Add()([x, input_tensor])
     x = Activation('relu')(x)
@@ -74,19 +74,19 @@ def identity_block_td(input_tensor, kernel_size, filters, stage, block, trainabl
 
     x = TimeDistributed(Convolution3D(nb_filter1, (1, 1, 1), trainable=trainable, kernel_initializer='normal'),
                         name=conv_name_base + '2a')(input_tensor)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2a')(x)
+    x = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
 
     x = TimeDistributed(
         Convolution3D(nb_filter2, (kernel_size, kernel_size, kernel_size), trainable=trainable,
                       kernel_initializer='normal',
                       padding='same'), name=conv_name_base + '2b')(x)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2b')(x)
+    x = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
 
     x = TimeDistributed(Convolution3D(nb_filter3, (1, 1, 1), trainable=trainable, kernel_initializer='normal'),
                         name=conv_name_base + '2c')(x)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2c')(x)
+    x = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '2c')(x)
 
     x = Add()([x, input_tensor])
     x = Activation('relu')(x)
@@ -103,20 +103,20 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2),
 
     x = Convolution3D(nb_filter1, (1, 1, 1), strides=strides, name=conv_name_base + '2a', trainable=trainable)(
         input_tensor)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
 
     x = Convolution3D(nb_filter2, (kernel_size, kernel_size, kernel_size), padding='same', name=conv_name_base + '2b',
                       trainable=trainable)(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
 
     x = Convolution3D(nb_filter3, (1, 1, 1), name=conv_name_base + '2c', trainable=trainable)(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+    x = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
 
     shortcut = Convolution3D(nb_filter3, (1, 1, 1), strides=strides, name=conv_name_base + '1', trainable=trainable)(
         input_tensor)
-    shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+    shortcut = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
 
     x = Add()([x, shortcut])
     x = Activation('relu')(x)
@@ -135,23 +135,23 @@ def conv_block_td(input_tensor, kernel_size, filters, stage, block, input_shape,
     x = TimeDistributed(
         Convolution3D(nb_filter1, (1, 1, 1), strides=strides, trainable=trainable, kernel_initializer='normal'),
         input_shape=input_shape, name=conv_name_base + '2a')(input_tensor)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2a')(x)
+    x = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
 
     x = TimeDistributed(
         Convolution3D(nb_filter2, (kernel_size, kernel_size, kernel_size), padding='same', trainable=trainable,
                       kernel_initializer='normal'), name=conv_name_base + '2b')(x)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2b')(x)
+    x = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
 
     x = TimeDistributed(Convolution3D(nb_filter3, (1, 1, 1), kernel_initializer='normal'), name=conv_name_base + '2c',
                         trainable=trainable)(x)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2c')(x)
+    x = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '2c')(x)
 
     shortcut = TimeDistributed(
         Convolution3D(nb_filter3, (1, 1, 1), strides=strides, trainable=trainable, kernel_initializer='normal'),
         name=conv_name_base + '1')(input_tensor)
-    shortcut = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '1')(shortcut)
+    shortcut = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '1')(shortcut)
 
     x = Add()([x, shortcut])
     x = Activation('relu')(x)
@@ -175,7 +175,7 @@ def nn_base(input_tensor=None, trainable=False):
     x = ZeroPadding3D((3, 3, 3))(img_input)
 
     x = Convolution3D(64, (7, 7, 7), strides=2, name='conv1', trainable=trainable)(x)
-    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = FixedBatchNormalization(axis=bn_axis, name='bn_conv1')(x)
     x = Activation('relu')(x)
     x = MaxPooling3D((3, 3, 3), strides=2)(x)
 
