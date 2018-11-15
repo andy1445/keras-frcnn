43f56a1435bb44a375dd1f2653bc15da9c8e936b
andy1445 <gaoandy1445@gmail.com>
2018-11-14 22:16:44 -0500

    added more data processing + 3d

--- CNNinputDataExtractionV3.py
+++ CNNinputDataExtractionV3.py
@@ -40,69 +40,6 @@ print("Start")
 slicefail = False
 
 
-# def createSample(listp, dictp, zcenter):
-#     # creates a sample from inputbox, imageDict, zcenter
-#     listToReturn = []
-#     slicesfound = 0  # for debugging
-#     centerZIndex = None
-#     # create volume and list of minz maxz
-#     if zcenter in dictp:
-#         centerZIndex = list(dictp.keys()).index(zcenter)
-#     minZIndex = int(centerZIndex - Zsize / 2)
-#     maxZIndex = int(centerZIndex + Zsize / 2)
-#     if minZIndex < 0 or maxZIndex > len(dictp):  # for debugging
-#         print("Slice out of range")
-#         slicefail = True
-#         return ([], [])
-#     zlist = list(dictp.items())
-#     minzbound = float(zlist[minZIndex][0])
-#     maxzbound = float(zlist[maxZIndex][0])
-#     for j in range(minZIndex, maxZIndex):
-#         value = zlist[j][1]
-#         part = np.array(value)
-#         slicewanted = part[listp[0][0]:listp[0][1], listp[1][0]:listp[1][1]]
-#         listToReturn.append(slicewanted)
-#     return (listToReturn, [minzbound, maxzbound])
-#
-#
-# def CreateTranslatedPositive(listp, dictp, zcenter):
-#     # boxXY, imageDict, centerZ
-#     # Randomized translation:
-#     xShift = random.randint(-10, 10)
-#     yShift = random.randint(-10, 10)
-#     zShift = random.randint(-4, 4)
-#
-#     listp[0][0] += xShift
-#     listp[0][1] += xShift
-#     listp[1][0] += yShift
-#     listp[1][1] += yShift
-#
-#     listToReturn = []
-#     slicesfound = 0  # for debugging
-#     # check z translate range
-#     centerZIndex = None
-#     if zcenter in dictp:
-#         centerZIndex = list(dictp.keys()).index(zcenter)
-#         centerZIndex += zShift
-#     minZIndex = int(centerZIndex - Zsize / 2)
-#     maxZIndex = int(centerZIndex + Zsize / 2)
-#     if minZIndex < 0 or maxZIndex > len(dictp):  # for debugging
-#         print("Slice out of range")
-#         slicefail = True
-#         return ([], [])
-#     zlist = list(dictp.items())
-#     minzbound = float(zlist[minZIndex][0])
-#     maxzbound = float(zlist[maxZIndex][0])
-#     # append slices to create volume
-#     for k in range(minZIndex, maxZIndex):
-#         value = zlist[k][1]
-#         part = np.array(value)
-#         slicewanted = part[listp[0][0]:listp[0][1], listp[1][0]:listp[1][1]]
-#         listToReturn.append(slicewanted)
-#     # return volume, [minz, maxz]
-#     return (listToReturn, [minzbound, maxzbound])
-
-
 def getZindices(imageDict, minz, maxz):
     minZIndex = list(imageDict.keys()).index(minz)
     maxZIndex = list(imageDict.keys()).index(maxz)
@@ -114,39 +51,6 @@ def getZindices(imageDict, minz, maxz):
     return list(imageDict.keys()).index(minz), list(imageDict.keys()).index(maxz)
 
 
-# functions
-# def createNegative(listp, dictp, slthick):
-#     # listp = exclude_set, dictp = imageDict, slice thickness
-#     while (True):
-#         # get xmin and y min
-#         xmin = random.randint(0, 512 - Xsize)
-#         ymin = random.randint(0, 512 - Ysize)
-#         # get z from valid zs
-#         allZs = list(dictp.keys())
-#         validZs = allZs[int(0 + .5 * Zsize): int(len(allZs) - (.5 * Zsize))]
-#         zcenter = random.choice(validZs)
-#         zmin = zcenter - slthick * .5 * Zsize
-#         intersect = False
-#         for box in listp:
-#             xcoords = box[0]
-#             ycoords = box[1]
-#             zcoords = box[2]
-#             # check if xmin, ymin, zmin, in range of nodule
-#             if xmin in range(xcoords[0] - Xsize, xcoords[1]):
-#                 if ymin in range(ycoords[0] - Ysize, ycoords[1]):
-#                     if zmin >= zcoords[0] - slthick * Zsize and zmin <= zcoords[1]:
-#                         intersect = True
-#                         break
-#         # if no intersections, add a negative sample
-#         if (not intersect):
-#             samplex = [xmin, xmin + Xsize]
-#             sampley = [ymin, ymin + Ysize]
-#             inputBox = [samplex, sampley]
-#             return createSample(inputBox, dictp, zcenter)
-
-
-# init main
-# load database information
 fileextnesion = '.pickle'
 loadedData = None
 
@@ -157,27 +61,24 @@ allNodules = x1.parse(x1.sheet_names[0])
 allNodules = allNodules.sort_values(['SeriesID'])
 
 IDs = list(allNodules["SeriesID"].drop_duplicates())
-# validation_set = IDs[-120:-1]
-# validation_set.append(IDs[-1])
 validation_set = []
-# print(sorted(validation_set) == validation_set); exit()
 
 nodulesToUse = x1.parse(x1.sheet_names[2])
 noduleSet = set(nodulesToUse["NoduleID"])
 del nodulesToUse
 del x1
 
-# save all nodules in numpy form
-for id in IDs:
-    print(id)
-    with open(savePath + str(id), "rb") as f:
-        loadedData = pickle.load(f)
-        imageDict = OrderedDict(sorted(loadedData.items(), key=lambda t: t[0]))
-        npmatrix = np.array(list(imageDict.values()))
-
-        npmatrix = np.transpose(npmatrix, (1, 2, 0))
-        np.save(savePath + str(id) + '.npy', npmatrix)
-        print(npmatrix.shape)
+# save all nodules in numpy form, uncomment if need to reprocess
+# for id in IDs:
+#     print(id)
+#     with open(savePath + str(id), "rb") as f:
+#         loadedData = pickle.load(f)
+#         imageDict = OrderedDict(sorted(loadedData.items(), key=lambda t: t[0]))
+#         npmatrix = np.array(list(imageDict.values()))
+#
+#         npmatrix = np.transpose(npmatrix, (1, 2, 0))
+#         np.save(savePath + str(id) + '.npy', npmatrix)
+#         print(npmatrix.shape)
 
 # headerList = list(allNodules)
 prevID = None
@@ -193,9 +94,6 @@ imageDict = None
 takeNegativeSample = True
 seriesIDset = set()
 counterx = 0
-# print ('val_set index',[list(allNodules['SeriesID']).index(validation_set[i]) for i in range(len(validation_set))])
-# print ('all_set index',[list(allNodules['SeriesID']).index(allNodules['SeriesID'][i]) for i in range(len(list(allNodules['SeriesID'])))])
-# iterate thru nodules w/ repetitions of ids
 
 
 for i in range(len(allNodules)):
@@ -249,35 +147,3 @@ print("Negative samples: " + str(len(negativelist)))
 with open("data.txt", 'w') as f:
     for line in positivelist:
         f.write(','.join(line) + '\n')
-
-# print (counterx)
-# print (counterzeta)
-# with open(savePath + "PositiveAugmented.pickle", 'wb') as handle:
-#     # x_train = np.array(positivelist)
-#     # x_train = np.clip(x_train, bottom_threshold, top_threshold)
-#     # x_train = x_train.reshape(
-#     #    (x_train.shape[0], x_train.shape[1], x_train.shape[2], x_train.shape[3], 1))
-#     # minx = np.amin(x_train)
-#     # halfRange = (np.amax(x_train) - minx) / 2.0
-#     # if minx < 0:
-#     #    x_train -= minx
-#     # x_train = (x_train - halfRange) / halfRange
-#     # pickle.dump(x_train, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#     pickle.dump(positivelist, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-# with open(savePath + "NegativeAugmented.pickle", 'wb') as handle:
-#     pickle.dump(negativelist, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#
-# with open(savePath + "ValidationAugmented.pickle", 'wb') as handle:
-#     pickle.dump(validation_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#
-# with open(savePath + "PositiveerrorsAugmented.pickle", 'wb') as handle:
-#     pickle.dump(pfailedposlist, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#     pickle.dump(sfailedposlist, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#
-# with open(savePath + "NegativeerrorsAugmented.pickle", 'wb') as handle:
-#     pickle.dump(pfailedneglist, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#     pickle.dump(sfailedneglist, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#
-# with open(savePath + "ValidationIDs4.pickle", 'wb') as handle:
-#     pickle.dump(validation_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
